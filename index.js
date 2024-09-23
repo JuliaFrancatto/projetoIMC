f81a4a4abb9c9a654d13b342d6b288a230a882c8
JuliaFrancatto <juliavitoria_800@hotmail.com>
2024-09-23 20:19:18 -0300

    projeto atualizado

--- index.js
+++ index.js
@@ -61,53 +61,52 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
  *                   type: string
  */
 
-
-// pegando a variarel app e setando um get nela para definir os parametros para desenvolver a API.
+//difinindo os parametros para o desenvolvimento da API com a variavel
 app.get('/imc', (req, res, next) => {
     try {
         const { peso, altura} = req.query;
 
 
-        // definindo erro se nenhum parametro for informado no React.
+        // informar erro se não houver parametro.
         if (peso === undefined || altura === undefined ) {
             throw new Error('Parâmetros insuficientes!');
         }
 
-        // definindo que pesoNumber e alturaNumber são numeros reais e serão utilizados em calculos.
+        //difinindo os números reais para serem utilizados nos cálculos. 
         const pesoNumber = parseFloat(peso);
         const alturaNumber = parseFloat(altura);
 
-       //Se o valor não ser um numero será retonado "Parametros inválidos".
+       //Retornar "Parametros inválidos" se não tiver valor informados.
         if (isNaN(pesoNumber) || isNaN(alturaNumber)) {
             throw new Error('Parâmetros inválidos!');
         }
 
-        //Definindo a variavel imc como imutavel, pois trata-se de uma operação matematica.
+        
         const imc = peso / (altura * altura);
 
-        //Definindo uma variavel para receber os resultados da conta imc
+        //Definindo uma variavel para  resultados da conta
         let result1;
          
 
-        //Passando if para analisar o resultado e retornar uma mensagem caso o imc dê determinados resultados. 
+        
+        //Utilizando o if para os resultados, para retornar a mensagem com o valor determinado do IMC
         if (imc < 18.5) {
             result1 = `${imc} - Abaixo do peso`;
         } else if (imc >= 18.5 && imc < 24.9) {
             result1 = `${imc} - Peso normal`;
         } else if (imc >= 25 && imc < 29.9) {
             result1 = `${imc} - Sobrepeso`;
-        } else { 
+        } else {  
             result1 = `${imc} - Obesidade`;
         }
 
-        //Passando res.json para devolver o resultado tanto do calculco imc quanto do if no navegador ou na propria aplicação.
-        res.json({result1});
+        //devolvendo o resultado tanto do calculco imc quanto do if
     } catch (error) {
         next(error); 
     }
 });
 
-// Middleware de tratamento de erros
+// tratamento de erros
 app.use((err, req, res, next) => {
     console.error(err.stack); // Log do erro
     res.status(400).json({ error: err.message }); // Responde com a mensagem de erro
